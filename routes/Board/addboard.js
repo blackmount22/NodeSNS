a54a013cd4094bda0ff86d985b7fe975acd29180
DESKTOP-ODK3A0K\jhw <jjhw0106@naver.com>
2022-06-06 01:09:40 +0900

    패키지 구조 리팩토링_협의필요

--- routes/Board/addboard.js
+++ routes/Board/addboard.js
@@ -5,7 +5,10 @@ const {addBoard} = require('../../model/board')
 
 router.post('/', async (req,res,next) => {
     let result = await addBoard();
+    // res.render('board/addBoard.html');
     console.log(result);
 })
-
+router.get('/', (req, res) => {
+    res.render('board/addBoard.html');
+});
 module.exports = router;
